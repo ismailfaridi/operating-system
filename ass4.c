e875eed6bb4baf177767e35bd32ac5e414a2013f
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-05-12 18:41:47 +0500

    Update ass4.c

--- ass4.c
+++ ass4.c
@@ -1,58 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <pthread.h> // pthread_create(), pthread_join(), pthread_exit()
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LENGTH 100
+#define MAX_LENGTH 50 // max limit of name input
 
 // Thread 1: Reads integers from input.txt, calculates sum, writes to output.txt & console
-void *thread1_function(void *arg)
+void *thread1_func(void *arg)
 {
     FILE *inFile = fopen("input.txt", "r");
+
     if (inFile == NULL)
     {
-        perror("Error opening input.txt");
+        perror("Error while opening input.txt file.");
         pthread_exit(NULL);
     }
-    int sum = 0, number;
+
+    int sum = 0, num;
 
     // Read 5 integers from file
     for (int i = 0; i < 5; i++)
     {
-        if (fscanf(inFile, "%d", &number) != 1)
+        if (fscanf(inFile, "%d", &num) != 1)
         {
-            printf("Error reading integer from input.txt\n");
+            printf("Error while reading integer from input.txt file.\n");
             fclose(inFile);
             pthread_exit(NULL);
         }
-        sum += number;
+
+        sum += num;
     }
 
     fclose(inFile);
 
     // Print sum to console
-    printf("Sum calculated by Thread T1: %d\n", sum);
+    printf("T1: Sum of numbers: %d\n", sum);
 
     // Write sum to output.txt
     FILE *outFile = fopen("output.txt", "w");
+
     if (outFile == NULL)
     {
-        perror("Error opening output.txt");
+        perror("Error while opening output.txt file.");
         pthread_exit(NULL);
     }
 
-    fprintf(outFile, "Sum: %d\n", sum);
+    fprintf(outFile, "Sum of numbers: %d\n", sum);
     fclose(outFile);
 
     pthread_exit(NULL);
 }
 
 // Thread 2: Takes user name input, replaces vowels, prints and appends to output.txt
-void *thread2_function(void *arg)
+void *thread2_func(void *arg)
 {
     char input[MAX_LENGTH];
-    printf("Enter your full name: ");
+
+    printf("T2: Enter a string: ");
     fgets(input, MAX_LENGTH, stdin);
 
     // Remove newline character if present
@@ -68,17 +73,18 @@ void *thread2_function(void *arg)
     }
 
     // Print modified name
-    printf("Modified String by Thread T2: %s\n", input);
+    printf("T2: Modified string: %s\n", input);
 
     // Append modified string to output.txt
     FILE *outFile = fopen("output.txt", "a");
+
     if (outFile == NULL)
     {
-        perror("Error opening output.txt");
+        perror("Error while opening output.txt file.");
         pthread_exit(NULL);
     }
 
-    fprintf(outFile, "Modified Name: %s\n", input);
+    fprintf(outFile, "Modified string: %s\n", input);
     fclose(outFile);
 
     pthread_exit(NULL);
@@ -87,35 +93,38 @@ void *thread2_function(void *arg)
 int main()
 {
     pthread_t t1, t2;
+
     // Check if input.txt exists, create if it doesn't
-    FILE *checkFile = fopen("input.txt", "r");
-    if (checkFile == NULL)
+    FILE *inFile = fopen("input.txt", "r");
+
+    if (inFile == NULL)
     {
-        checkFile = fopen("input.txt", "w");
-        if (checkFile != NULL)
+        inFile = fopen("input.txt", "w");
+
+        if (inFile != NULL)
         {
             // Write 5 integers to file
-            fprintf(checkFile, "10\n20\n30\n40\n50\n");
-            printf("Created input.txt with default values.\n");
-            fclose(checkFile);
+            fprintf(inFile, "10\n20\n30\n40\n50\n");
+            // printf("Created input.txt with default values.\n");
+            fclose(inFile);
         }
         else
         {
-            perror("Unable to create input.txt");
+            perror("Unable to create the input.txt file.");
             return 1;
         }
     }
     else
     {
-        fclose(checkFile);
+        fclose(inFile);
     }
 
     // Create and run Thread 1
-    pthread_create(&t1, NULL, thread1_function, NULL);
+    pthread_create(&t1, NULL, thread1_func, NULL);
     pthread_join(t1, NULL); // Wait for Thread 1 to finish
 
     // Create and run Thread 2
-    pthread_create(&t2, NULL, thread2_function, NULL);
+    pthread_create(&t2, NULL, thread2_func, NULL);
     pthread_join(t2, NULL); // Wait for Thread 2 to finish
 
     return 0;

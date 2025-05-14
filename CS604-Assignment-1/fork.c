adaea3801b4155023c857792ec535e54359a8bca
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-05-14 15:16:26 +0500

    fork

---
+++ CS604-Assignment-1/fork.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int factorial(int n) {
+  if (n < 1) return 1;
+  return n * factorial(n - 1);
+}
+
+int main() {
+  pid_t pid;
+  int max_child = 3;
+  int num[] = {3, 4, 5};
+
+  for(int i=0; i<max_child; i++) {
+    pid = fork();
+    
+    if(pid < 0) {
+      perror("Fork Failed");
+      exit(1); // program terminate unsuccessfully
+    }
+
+    // Child Processes
+    if(pid == 0) {
+      int fac = factorial(num[i]);
+
+      printf("Child PID: %d\n", getpid());
+      printf("Parent PID: %d\n", getppid());
+      printf("BC220406016 - Muhammad Ismail\n");
+      printf("Factorial of %d is %d\n\n", num[i], fac);
+      exit(0);
+    }
+  }
+    
+  // Parent Process
+  for(int i=0; i<max_child; i++) {
+    wait(NULL);
+  }
+
+  printf("PARENT: All Child Processes are completed\n");
+
+  return 0;
+}
